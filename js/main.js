73ab46520dc09edc1aaf63ea9f263ce7c184adec
xfreed <xfreed1@gmail.com>
2019-09-16 20:33:15 +0300

    Update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+new fullpage('#fullpage', {
+    navigation: true,
+    responsiveWidth: 700,
+    // anchors: ['Home', 'Introduction', 'Gallery', 'Contact'],
+    parallax: true,
+    // onLeave: function(origin, destination, direction) {
+    //     console.log("Leaving section" + origin.index);
+    // },
+    navigationTooltips: ['Home', 'Introduction', 'Gallery', 'Contact'],
+    showActiveTooltip: false,
+
+});
